f70ab38b588988e920aebeff54a92c945548ddaf
AdnannM <adnannmuratovic@gmx.com>
2020-06-17 17:38:49 +0200

    TableView Custom protocol model display vidio title...

---
+++ youtube-App/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  youtube-App
+//
+//  Created by Adnann Muratovic on 01/06/2020.
+//  Copyright © 2020 Adnann Muratovic. All rights reserved.
+//
+
+import Foundation
+
+struct Constants: Codable {
+	
+	private static var API_KEY = "AIzaSyB1nKErDJKQgZ8TA7vjiYa4PYgPeZsYqnc"
+	static var PLAYLIST_ID = "UUB_qr75-ydFVKSF9Dmo6izg"
+	static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+	static var videoCell_ID = "VideoCell"
+	
+}
